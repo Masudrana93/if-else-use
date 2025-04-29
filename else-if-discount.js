5b2ee3a8dc79048ec12cea9ecfb9cae36095b5c7
Masud Rana <mr007masudrana@gmail.com>
2025-04-29 17:36:02 +0600

    else-if for discount

---
+++ else-if-discount.js
@@ -0,0 +1,16 @@
+// ******Dicount Kheko if-else******
+
+const price = 6500;
+
+if(price >= 5000){
+    const discount = price / 100 * 10;
+    const pay = price - discount;
+    console.log(pay);
+}else{
+    console.log(price);
+}
+
+// Practice:
+
+// 1. 
+
